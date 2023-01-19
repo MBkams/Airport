af8df90051217647978e2d2ee429fb631e0386d5
Bilal Marecar <billal.marecar@gmail.com>
2023-01-19 15:08:49 +0100

    Interface IHM feat : La planète Terre qui tourne avec affichage des coordonées des aéroports à proximité

--- SRC/Earth.java
+++ SRC/Earth.java
@@ -1,54 +1,148 @@
 package SRC;
 
+import java.util.ArrayList;
+import javafx.animation.AnimationTimer;
+import javafx.geometry.Point3D;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
+
 /**
  * La classe Earth est héritiaire de la classe Group.
  * Elle affiche un modèle de la Terre tournant sur elle-meme sur une fenetre JavaFx.
+ * 
  */
 
 public class Earth extends Group{
 
-//Le rayon de rotation de la terre
-private Rotate ry;
+  //Le rayon de rotation de la terre
+  private Rotate ry = new Rotate();
 
-//L'objet Sphere representant la Terre
-private Sphere sph;
+  /**
+   * Obtient l'objet Sphere représentant la Terre.
+   * @return l'objet Sphere représentant la Terre.
+  */
+  public Sphere getEarth(){
+      return sph;
+  };
 
-//Une liste de sphere 
-private ArrayList<Sphere> yellowSphere = new ArrayList<Sphere>();
+  //L'objet Sphere representant la Terre
+  private Sphere sph; 
+
+  //Une liste de sphere jaunes 
+  private ArrayList<Sphere> yellowSphere;
 
    /**
-   * Contructeur de la classe Earth
-   *
-   * @param rayon Le rayon de rotation de la Terre autour de son axe
-   * @param sphere L'objet Sphere représentant la Terre
+   * Contructeur de la classe Earth qui définit une représentation 3D sphérique de la Terre.
+   * 
    */
-   public Earth(Rotate rayon,Sphere sphere){
-      this.ry = rayon;
-      this.sph = sphere;
+    public Earth(){
+
+      //Initialise la liste des spheres jaunes
+      yellowSphere = new ArrayList<Sphere>();
+      
+      //Initialise la sphere représentant la Terre avec 300 pixels de rayon
+      sph = new Sphere(300);
+
+      //Création d'un matériau de type ombrage de Phong
+      PhongMaterial material = new PhongMaterial();
+      
+      /*
+      * Mapping de l'image, radiance, texture, réflexion sur le matériel
+      */
+      material.setDiffuseMap(new Image("file:./data/earth_lights_4800.png"));
+      material.setBumpMap(new Image("file:./data/earth_lights_4800.png"));
+      material.setSpecularMap(new Image("file:./data/earth_lights_4800.png"));
+      material.setSelfIlluminationMap(new Image("file:./data/earth_lights_4800.png"));
+
+      //Mapping du matériel sur le sphère
+      sph.setMaterial(material);
 
       //Ajout de la classe construteur Earth au objet Group
       this.getChildren().add(sph);
 
-        // Modification de l'objet Rotate sur l'axe Y
-       ry.setAxis(ROTATE.Y_AXIS);
+      //Ajout du rayon sur la classe Earth
+      this.getTransforms().add(ry);
 
-       AnimationTimer animationTimer = new AnimationTimer() {
-       @Override
-       public void handle(long time) {
-         System.out.println("Valeur de time : " + time);
-         ry.setAngle(...); // A compl ́eter
-       }
-       };
-       animationTimer.start();
+      /*
+       * Mise en rotation de la Terre avec la classe AnimatationTimer
+       */
+      AnimationTimer animationTimer = new AnimationTimer() {
+        @Override
+        public void handle(long time) {
+          //System.out.println("Valeur de time : " + time);
+          // Rotation sur l'axe Y pour obtenir une revolution
+          ry.setAxis(new Point3D(0, 1, 0));
 
-   }
+          //Vitesse de rotation de l'angle par rapport au temps
+          ry.setAngle(time/(15*Math.pow(10, 7)));
+        }
+        };
+        animationTimer.start();
 
-     // Création d'un matériau Phong
-     PhongMaterial material = new PhongMaterial();
-     material.setDiffuseMap(new Image(getClass().getResourceAsStream("earth_lights_4800.png")));
-     
-     sph.setMaterial(material);
+    }
+    
+    /**
+       * Crée une nouvelle sphère avec une couleur et l'ajoute à la scène
+       * @param a : Objet Aeroport qui contient les valeurs de latitude et de longitude
+       * @param color : La couleur de la sphère
+       * @return : L'objet sphère créé
+       * 
+       */
+      public Sphere createSphere(Aeroport a,Color color){
+        
+        //Création d'un matériau de type ombrage de Phong
+        PhongMaterial mat = new PhongMaterial();
+        
+        /*
+         * Mapping de la couleur sur le matériel
+         */
+        mat.setSpecularColor(color);
+        mat.setDiffuseColor(color);
 
-      
+        //Creation d'un sphere de rayon 2
+        Sphere coloredSphere = new Sphere(2);
+
+        //Mapping du matériel sur le sphère
+        coloredSphere.setMaterial(mat);
+
+        //Récupération des coordonées GPS
+        double latitude=Math.toRadians(a.getLatitude()-13);
+        double longitude=Math.toRadians(a.getLongitude());
+
+        /**
+         * Correspondance aux valeurs de latitude et de longitude de l'aéroport
+         * @param latitude : La valeur de latitude en radians
+         * @param longitude : La valeur de longitude en radians
+         * @param coloredSphere : L'objet sphère à transformer
+         */
+        Translate Axez= new Translate(300.*Math.cos(latitude)*Math.sin(longitude),-300*Math.sin(latitude),-300*Math.cos(latitude)*Math.cos(longitude));
+        coloredSphere.getTransforms().add(Axez);
+
+        /**
+          * Rotation de la sphère autour de l'axe X pour correspondre à la latitude de l'aéroport
+          * @param Rotx : Rotation autour de l'axe X avec un angle de 40 degrés
+          */
+        Rotate Rotx = new Rotate (40,Rotate.X_AXIS);
+        coloredSphere.getTransforms().add(Rotx);
+
+       /**
+        * Rotation de la sphère autour de l'axe Y pour correspondre à la longitude de l'aéroport
+        * @param Roty : Rotation autour de l'axe Y avec un angle de 73 degrés
+        */
+        Rotate Roty = new Rotate (73,Rotate.Y_AXIS);
+        coloredSphere.getTransforms().add(Roty);
+
+        return coloredSphere;
+      }
+
+      public void displayRedSphere(Aeroport a){
+        this.getChildren().add(createSphere(a,Color.RED));
+    }
+   
 
 }
--- SRC/Interface.java
+++ SRC/Interface.java
@@ -1,50 +1,124 @@
 package SRC;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.PickResult;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
+import javafx.geometry.Point2D;
+import javafx.geometry.Point3D;
+
+
+/**
+ * La classe Interface affiche une fenêtre avec un modèle 3D de la Terre en rotation.
+ * Elle utilise la classe Earth pour créer un modèle 3D de la Terre, 
+ * ajoute une perspective de caméra pour permettre une vue en 3D,
+ * et gère les événements de la souris pour permettre une interaction avec l'utilisateur.
+ * 
+ */ 
 public class Interface extends Application {
 
-    private Earth earth;
-    private Rotate ry;
-    private Sphere sph;
+    //La translation de la Terre sur l'axe Z
+    Translate TransAxeZ = new Translate();
 
-@Override
-public void start(Stage primaryStage) throws Exception{
+    //Variabe de position horizontal X de la souris 
+    private double PosMouseX;
 
-    earth = new Earth(ry,sph):
-    
-    Scene theScene = new Scene(new Pane(earth), 600, 400,true);
+    //Variable de position vertical Y de la souris
+    private double PosMouseY;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+    // Instanciation de la classe World
+    World w = new World("./data/airport-codes_no_comma.csv");
 
     // Afichage de la fenetre
-    primaryStage.setTitle("Hello world");
-    primaryStage.setScene(theScene);
-    primaryStage.show();
+    primaryStage.setTitle("Bilal");
 
-    // Ajout d'une perspective de camera 
+    // Instanciation de la classe Earth
+    Earth earth = new Earth();
+    
+    // Creation de la scene
+    Scene theScene = new Scene(earth, 600, 400,true);
+
+    // Ajout d'une perspective de camera avec un recul de 300 pixels
     PerspectiveCamera camera = new PerspectiveCamera(true);
     camera.setTranslateZ(-1000);
     camera.setNearClip(0.1);
-    camera.setFarClip(2000.0);
-    camera.setFieldOfView(35);
+    camera.setFarClip(3000.0);
+    camera.setFieldOfView(50);
     theScene.setCamera(camera);
 
+     /*
+     * Gestion des evenemnts de la  souris (Clique Gauche)
+     * Permet une translation sur l'axe Z avec le clique gauche enfoncé de la souris
+     */
     theScene.addEventHandler(MouseEvent.ANY, event -> {
         if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
-            System.out.println("Clicked on : (" event.getSceneX()+ ", "+ event.getSceneY()+")");
-        }
-        if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
-            // Translation sur l'axe Z à vérifier
-            camera.getTransforms().add(new Translate(0,0,50)); 
-        }
-    };
+            System.out.println("Clicked on : ("+ event.getSceneX() + ", "+ event.getSceneY()+")");
+            PosMouseX = event.getSceneX();
+            PosMouseY = event.getSceneY();
 
-}
+        }
+        if (event.getButton() == MouseButton.PRIMARY && event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+            //Translation sur l'axe Z avc  une vitesse relative selon l'affichage
+            TransAxeZ.setZ((event.getSceneY() - PosMouseY)*0.001);
+            camera.getTransforms().add(TransAxeZ);
+        }
+    });
+
+
+    /**
+     * Gestion des evenemnts de la  souris (Clique Droit)
+     * Affichage de l'aeroport le plus proche selon le clique droit de la souris
+     */
+    theScene.addEventFilter(MouseEvent.ANY, event -> {
+        if (event.getButton() == MouseButton.SECONDARY && event.getEventType() == MouseEvent.MOUSE_CLICKED)
+        {
+            PickResult pickResult = event.getPickResult();
+            if (pickResult.getIntersectedNode() != null)
+            {
+                //Point d'intersection des coordoonées du point cliqués sur la souris
+                Point2D click = pickResult.getIntersectedTexCoord();
+
+                /**
+                 * Conversion en coordonées GPS des points cliqués à un instant t avec 
+                 * les méthodes getX,getY de la classe javafx.geometry.Point2D
+                 */
+                double latitude=2*Math.toDegrees(Math.atan(Math.exp((0.5-click.getY())/0.2678))-(Math.PI/4));
+                double longitude=360*(click.getX()-0.5);
+                
+                //Recherche dans l'objet w de la classe World de l'aeeroport le plus proche.
+                Aeroport apt_near = w.findNearestAirport(latitude, longitude);
+
+                earth.displayRedSphere(apt_near);
+
+                //Affichage sur la console de l'aeroport le plus proche
+                System.out.println(apt_near.toString());
+  
+            }
+        }
+
+    });
+
+
+    //Affichage de la scène 
+    primaryStage.setScene(theScene);
+    primaryStage.show();
+
+    }
+
+/**
+ * Point d'entrée de l'application
+ * @param args paramètres de lancement de l'application
+ */
     public static void main(String[] args) {
     launch(args);
-}
+    }
 }
 
